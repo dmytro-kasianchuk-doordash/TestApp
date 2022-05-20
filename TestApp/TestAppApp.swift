546f6b46a9f377b4368e0621194bff93619cd4bb
dmytro-kasianchuk-doordash <dmytro.kasianchuk@doordash.com>
2022-05-20 08:50:51 -0700

    initial

---
+++ TestApp/TestAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestAppApp.swift
+//  TestApp
+//
+//  Created by Dmytro Kasianchuk on 5/19/22.
+//
+
+import SwiftUI
+
+@main
+struct TestAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
